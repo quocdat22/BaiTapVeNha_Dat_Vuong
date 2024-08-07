543995f419aa7e78edce63d93e5f6a04e883a68b
Hoang Le Quoc Dat <hoanglequocdat132002@gmail.com>
2024-08-07 20:27:50 +0700

    chuong2_13_1.Sắp xếp các phần tử trong ma trận sao cho dòng chẵn tăng dòng lẻ giảm.

---
+++ BaiTaiVeNha_Dat_Vuong/BaiTaiVeNha_Dat_Vuong/13.cpp
@@ -0,0 +1,153 @@
+#include <stdio.h>
+
+// Hàm hoán đổi hai phần tử
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Hàm sắp xếp một hàng tăng dần
+void sortRowAscending(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                swap(&arr[i], &arr[j]);
+            }
+        }
+    }
+}
+
+// Hàm sắp xếp một hàng giảm dần
+void sortRowDescending(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] < arr[j]) {
+                swap(&arr[i], &arr[j]);
+            }
+        }
+    }
+}
+
+// Bài 1: Sắp xếp các dòng chẵn tăng, dòng lẻ giảm
+void sortRows(int a[100][100], int n, int m) {
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            sortRowAscending(a[i], m);
+        }
+        else {
+            sortRowDescending(a[i], m);
+        }
+    }
+}
+
+// Hàm sắp xếp một cột tăng dần
+void sortColumnAscending(int a[100][100], int n, int col) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i][col] > a[j][col]) {
+                swap(&a[i][col], &a[j][col]);
+            }
+        }
+    }
+}
+
+// Hàm sắp xếp một cột giảm dần
+void sortColumnDescending(int a[100][100], int n, int col) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i][col] < a[j][col]) {
+                swap(&a[i][col], &a[j][col]);
+            }
+        }
+    }
+}
+
+// Bài 2: Sắp xếp các cột chẵn tăng, cột lẻ giảm
+void sortColumns(int a[100][100], int n, int m) {
+    for (int j = 0; j < m; j++) {
+        if (j % 2 == 0) {
+            sortColumnAscending(a, n, j);
+        }
+        else {
+            sortColumnDescending(a, n, j);
+        }
+    }
+}
+
+// Hàm sắp xếp ma trận tăng dần/giảm dần theo zic-zac
+void sortMatrixZigzag(int a[100][100], int n, int m, int ascending) {
+    int temp[4 * 5];
+    int k = 0;
+
+    // Copy tất cả các phần tử vào mảng tạm thời
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            temp[k++] = a[i][j];
+        }
+    }
+
+    // Sắp xếp mảng tạm thời
+    for (int i = 0; i < k - 1; i++) {
+        for (int j = i + 1; j < k; j++) {
+            if (ascending ? temp[i] > temp[j] : temp[i] < temp[j]) {
+                swap(&temp[i], &temp[j]);
+            }
+        }
+    }
+
+    // Copy lại vào ma trận theo thứ tự zic-zac
+    k = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = temp[k++];
+            }
+        }
+        else {
+            for (int j = m - 1; j >= 0; j--) {
+                a[i][j] = temp[k++];
+            }
+        }
+    }
+}
+
+// Hàm in ma trận
+void printMatrix(int a[100][100], int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n = 4, m = 5; // Số hàng và số cột của ma trận
+    int a[100][100] = {
+        {28, 6, 3, 7, 1},
+        {6, 15, 2, 4, 3},
+        {4, 8, 496, 10, 12},
+        {2, 18, 22, 3, 6}
+    };
+
+    printf("Ma tran ban dau:\n");
+    printMatrix(a, n, m);
+
+    // Bài 1: Sắp xếp các phần tử trong ma trận sao cho dòng chẵn tăng dòng lẻ giảm
+    sortRows(a, n, m);
+    printf("\nBai 1: Ma tran sau khi sap xep cac dong chan tang, dong le giam:\n");
+    printMatrix(a, n, m);
+
+    // Bài 2: Sắp xếp các phần tử trong ma trận sao cho cột chẵn tăng cột lẻ giảm
+    sortColumns(a, n, m);
+    printf("\nBai 2: Ma tran sau khi sap xep cac cot chan tang, cot le giam:\n");
+    printMatrix(a, n, m);
+
+    // Bài 3: Sắp xếp ma trận tăng dần theo zic-zac
+    sortMatrixZigzag(a, n, m, 1);
+    printf("\nBai 3: Ma tran sau khi sap xep tang dan theo zic-zac:\n");
+    printMatrix(a, n, m);
+
+    return 0;
+}
--- BaiTaiVeNha_Dat_Vuong/BaiTaiVeNha_Dat_Vuong/BaiTap12.cpp
+++ BaiTaiVeNha_Dat_Vuong/BaiTaiVeNha_Dat_Vuong/BaiTap12.cpp
@@ -1,190 +1,190 @@
-#include <stdio.h>
-#include <limits.h>
-
-int isPerfectNumber(int n) {
-    int sum = 0;
-    for (int i = 1; i <= n / 2; i++) {
-        if (n % i == 0) {
-            sum += i;
-        }
-    }
-    return sum == n;
-}
-
-int isPrime(int n) {
-    if (n < 2) return 0;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return 0;
-    }
-    return 1;
-}
-
-//in mat tran
-void printMatrix(int a[100][100], int m, int n) {
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%d ", a[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-int main() {
-    int m = 4;
-    int n = 5; // Số hàng và số cột của ma trận
-    int a[100][100] = {
-        {28, 6, 3, 7, 1},
-        {6, 15, 2, 4, 3},
-        {4, 8, 496, 10, 12},
-        {2, 18, 22, 3, 6}
-    };
-
-    // Bài 1: Liệt kê các cột có tổng nhỏ nhất trong ma trận
-    int minSum = INT_MAX;
-    int sum[5];
-    for (int j = 0; j < n; j++) {
-        sum[j] = 0;
-        for (int i = 0; i < m; i++) {
-            sum[j] += a[i][j];
-        }
-        if (sum[j] < minSum) {
-            minSum = sum[j];
-        }
-    }
-
-    printf("Bai 1: Cac cot co tong nho nhat:\n");
-    printMatrix(a, m, n);
-    for (int j = 0; j < n; j++) {
-        if (sum[j] == minSum) {
-            printf("Cot %d\n", j);
-        }
-    }
-
-    // Bài 2: Liệt kê các dòng có nhiều số hoàn thiện nhất trong ma trận
-    printMatrix(a, m, n);
-    int maxPerfect = 0;
-    int countPerfect[4];
-    for (int i = 0; i < m; i++) {
-        countPerfect[i] = 0;
-        for (int j = 0; j < n; j++) {
-            if (isPerfectNumber(a[i][j])) {
-                countPerfect[i]++;
-            }
-        }
-        if (countPerfect[i] > maxPerfect) {
-            maxPerfect = countPerfect[i];
-        }
-    }
-
-    printf("\nBai 2: Cac dong co nhieu so hoan thien nhat:\n");
-    printMatrix(a, m, n);
-    for (int i = 0; i < m; i++) {
-        if (countPerfect[i] == maxPerfect) {
-            printf("Dong %d\n", i);
-        }
-    }
-
-    // Bài 3: Liệt kê chỉ số các dòng chứa toàn giá trị chẵn
-    printf("\nBai 3: Cac dong chua toan gia tri chan:\n");
-    printMatrix(a, m, n);
-    for (int i = 0; i < m; i++) {
-        int allEven = 1;
-        for (int j = 0; j < n; j++) {
-            if (a[i][j] % 2 != 0) {
-                allEven = 0;
-                break;
-            }
-        }
-        if (allEven) {
-            printf("Dong %d\n", i);
-        }
-    }
-
-    // Bài 4: Tìm giá trị xuất hiện nhiều nhất trong ma trận
-    int maxCount = 0, maxValue;
-    int count[4 * 5];
-    int values[4 * 5];
-    int k = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            int value = a[i][j];
-            int found = 0;
-            for (int l = 0; l < k; l++) {
-                if (values[l] == value) {
-                    count[l]++;
-                    found = 1;
-                    if (count[l] > maxCount) {
-                        maxCount = count[l];
-                        maxValue = value;
-                    }
-                    break;
-                }
-            }
-            if (!found) {
-                values[k] = value;
-                count[k] = 1;
-                if (count[k] > maxCount) {
-                    maxCount = count[k];
-                    maxValue = value;
-                }
-                k++;
-            }
-        }
-    }
-    printMatrix(a, m, n);
-    printf("\nBai 4: Gia tri xuat hien nhieu nhat trong ma tran la: %d\n", maxValue);
-
-    // Bài 5: Tìm số nguyên tố nhỏ nhất trong ma trận
-    int minPrime = INT_MAX;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (isPrime(a[i][j]) && a[i][j] < minPrime) {
-                minPrime = a[i][j];
-            }
-        }
-    }
-    printMatrix(a, m, n);
-    if (minPrime == INT_MAX) {
-        printf("\nBai 5: Khong co so nguyen to trong ma tran.\n");
-    }
-    else {
-        printf("\nBai 5: So nguyen to nho nhat trong ma tran la: %d\n", minPrime);
-    }
-
-    // Bài 6: Tìm phần tử lớn (nhỏ) nhất trong dòng thứ k
-    printMatrix(a, m, n);
-    int k_row = 2; // Dòng thứ k
-    int maxInRow = INT_MIN;
-    int minInRow = INT_MAX;
-    for (int j = 0; j < n; j++) {
-        if (a[k_row][j] > maxInRow) {
-            maxInRow = a[k_row][j];
-        }
-        if (a[k_row][j] < minInRow) {
-            minInRow = a[k_row][j];
-        }
-    }
-
-    printf("\nBai 6: Phan tu lon nhat trong dong thu %d la: %d\n", k_row, maxInRow);
-    printf("Phan tu nho nhat trong dong thu %d la: %d\n", k_row, minInRow);
-
-    // Bài 7: Tìm phần tử lớn (nhỏ) nhất trong cột thứ k
-
-    int k_col = 1; // Cột thứ k
-    int maxInCol = INT_MIN;
-    int minInCol = INT_MAX;
-    for (int i = 0; i < m; i++) {
-        if (a[i][k_col] > maxInCol) {
-            maxInCol = a[i][k_col];
-        }
-        if (a[i][k_col] < minInCol) {
-            minInCol = a[i][k_col];
-        }
-    }
-    printMatrix(a, m, n);
-    printf("\nBai 7: Phan tu lon nhat trong cot thu %d la: %d\n", k_col, maxInCol);
-    
-    printf("Phan tu nho nhat trong cot thu %d la: %d\n", k_col, minInCol);
-    printMatrix(a, m, n);
-    return 0;
-}
+//#include <stdio.h>
+//#include <limits.h>
+//
+//int isPerfectNumber(int n) {
+//    int sum = 0;
+//    for (int i = 1; i <= n / 2; i++) {
+//        if (n % i == 0) {
+//            sum += i;
+//        }
+//    }
+//    return sum == n;
+//}
+//
+//int isPrime(int n) {
+//    if (n < 2) return 0;
+//    for (int i = 2; i * i <= n; i++) {
+//        if (n % i == 0) return 0;
+//    }
+//    return 1;
+//}
+//
+////in mat tran
+//void printMatrix(int a[100][100], int m, int n) {
+//	for (int i = 0; i < m; i++) {
+//		for (int j = 0; j < n; j++) {
+//			printf("%d ", a[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main() {
+//    int m = 4;
+//    int n = 5; // Số hàng và số cột của ma trận
+//    int a[100][100] = {
+//        {28, 6, 3, 7, 1},
+//        {6, 15, 2, 4, 3},
+//        {4, 8, 496, 10, 12},
+//        {2, 18, 22, 3, 6}
+//    };
+//
+//    // Bài 1: Liệt kê các cột có tổng nhỏ nhất trong ma trận
+//    int minSum = INT_MAX;
+//    int sum[5];
+//    for (int j = 0; j < n; j++) {
+//        sum[j] = 0;
+//        for (int i = 0; i < m; i++) {
+//            sum[j] += a[i][j];
+//        }
+//        if (sum[j] < minSum) {
+//            minSum = sum[j];
+//        }
+//    }
+//
+//    printf("Bai 1: Cac cot co tong nho nhat:\n");
+//    printMatrix(a, m, n);
+//    for (int j = 0; j < n; j++) {
+//        if (sum[j] == minSum) {
+//            printf("Cot %d\n", j);
+//        }
+//    }
+//
+//    // Bài 2: Liệt kê các dòng có nhiều số hoàn thiện nhất trong ma trận
+//    printMatrix(a, m, n);
+//    int maxPerfect = 0;
+//    int countPerfect[4];
+//    for (int i = 0; i < m; i++) {
+//        countPerfect[i] = 0;
+//        for (int j = 0; j < n; j++) {
+//            if (isPerfectNumber(a[i][j])) {
+//                countPerfect[i]++;
+//            }
+//        }
+//        if (countPerfect[i] > maxPerfect) {
+//            maxPerfect = countPerfect[i];
+//        }
+//    }
+//
+//    printf("\nBai 2: Cac dong co nhieu so hoan thien nhat:\n");
+//    printMatrix(a, m, n);
+//    for (int i = 0; i < m; i++) {
+//        if (countPerfect[i] == maxPerfect) {
+//            printf("Dong %d\n", i);
+//        }
+//    }
+//
+//    // Bài 3: Liệt kê chỉ số các dòng chứa toàn giá trị chẵn
+//    printf("\nBai 3: Cac dong chua toan gia tri chan:\n");
+//    printMatrix(a, m, n);
+//    for (int i = 0; i < m; i++) {
+//        int allEven = 1;
+//        for (int j = 0; j < n; j++) {
+//            if (a[i][j] % 2 != 0) {
+//                allEven = 0;
+//                break;
+//            }
+//        }
+//        if (allEven) {
+//            printf("Dong %d\n", i);
+//        }
+//    }
+//
+//    // Bài 4: Tìm giá trị xuất hiện nhiều nhất trong ma trận
+//    int maxCount = 0, maxValue;
+//    int count[4 * 5];
+//    int values[4 * 5];
+//    int k = 0;
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            int value = a[i][j];
+//            int found = 0;
+//            for (int l = 0; l < k; l++) {
+//                if (values[l] == value) {
+//                    count[l]++;
+//                    found = 1;
+//                    if (count[l] > maxCount) {
+//                        maxCount = count[l];
+//                        maxValue = value;
+//                    }
+//                    break;
+//                }
+//            }
+//            if (!found) {
+//                values[k] = value;
+//                count[k] = 1;
+//                if (count[k] > maxCount) {
+//                    maxCount = count[k];
+//                    maxValue = value;
+//                }
+//                k++;
+//            }
+//        }
+//    }
+//    printMatrix(a, m, n);
+//    printf("\nBai 4: Gia tri xuat hien nhieu nhat trong ma tran la: %d\n", maxValue);
+//
+//    // Bài 5: Tìm số nguyên tố nhỏ nhất trong ma trận
+//    int minPrime = INT_MAX;
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            if (isPrime(a[i][j]) && a[i][j] < minPrime) {
+//                minPrime = a[i][j];
+//            }
+//        }
+//    }
+//    printMatrix(a, m, n);
+//    if (minPrime == INT_MAX) {
+//        printf("\nBai 5: Khong co so nguyen to trong ma tran.\n");
+//    }
+//    else {
+//        printf("\nBai 5: So nguyen to nho nhat trong ma tran la: %d\n", minPrime);
+//    }
+//
+//    // Bài 6: Tìm phần tử lớn (nhỏ) nhất trong dòng thứ k
+//    printMatrix(a, m, n);
+//    int k_row = 2; // Dòng thứ k
+//    int maxInRow = INT_MIN;
+//    int minInRow = INT_MAX;
+//    for (int j = 0; j < n; j++) {
+//        if (a[k_row][j] > maxInRow) {
+//            maxInRow = a[k_row][j];
+//        }
+//        if (a[k_row][j] < minInRow) {
+//            minInRow = a[k_row][j];
+//        }
+//    }
+//
+//    printf("\nBai 6: Phan tu lon nhat trong dong thu %d la: %d\n", k_row, maxInRow);
+//    printf("Phan tu nho nhat trong dong thu %d la: %d\n", k_row, minInRow);
+//
+//    // Bài 7: Tìm phần tử lớn (nhỏ) nhất trong cột thứ k
+//
+//    int k_col = 1; // Cột thứ k
+//    int maxInCol = INT_MIN;
+//    int minInCol = INT_MAX;
+//    for (int i = 0; i < m; i++) {
+//        if (a[i][k_col] > maxInCol) {
+//            maxInCol = a[i][k_col];
+//        }
+//        if (a[i][k_col] < minInCol) {
+//            minInCol = a[i][k_col];
+//        }
+//    }
+//    printMatrix(a, m, n);
+//    printf("\nBai 7: Phan tu lon nhat trong cot thu %d la: %d\n", k_col, maxInCol);
+//    
+//    printf("Phan tu nho nhat trong cot thu %d la: %d\n", k_col, minInCol);
+//    printMatrix(a, m, n);
+//    return 0;
+//}
